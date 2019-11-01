7ae183be5d782a7a4272bfc778a9c70e4957a5cf
akshay-a-kulkarni <kulkarni.akshay@husky.neu.edu>
2019-11-01 17:22:54 -0400

    Scala Pagerank [COMPLETE]

--- PageRank-Spark/src/main/scala/pr/PageRank2.scala
+++ PageRank-Spark/src/main/scala/pr/PageRank2.scala
@@ -2,13 +2,10 @@ package pr
 
 import org.apache.log4j.LogManager
 import org.apache.spark.HashPartitioner
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{Dataset, SparkSession}
-import org.apache.spark.sql.functions._
-import org.apache.spark.sql.Encoders
+import org.apache.spark.sql.SparkSession
 
 
-object PageRank {
+object PageRank2 {
 
   // Creating a case class for defining edges.
   final case class Graph (
@@ -37,58 +34,44 @@ object PageRank {
 
     val partitioner = new HashPartitioner(1)
 
-    val k = 2
     // creating graph rdd
-    val graph = spark.sparkContext.parallelize(generateGraph(k))
+    val graph = spark.sparkContext.parallelize(generateGraph(3))
                                   .partitionBy(partitioner).persist()
-
-    val sink = spark.sparkContext.parallelize(List((0,0.0))).partitionBy(partitioner)
-
 
     val adj_graph = graph.groupByKey()
 
+    val sink = spark.sparkContext.parallelize(List((0,0.0))).partitionBy(partitioner)
 
     // Initialize each page's rank;
     // Use of mapValues ensure similar partitioning
-    var ranks = adj_graph.mapValues(v => 1.0/(k*k).toDouble).persist()
+    var ranks = graph.mapValues(v => 1.0/9.0)
 
     //adding sink node and its pr to list
     ranks = ranks.union(sink)
 
-    val num_nodes = adj_graph.count()
+    val num_nodes = ranks.count()
     var dist_pr:Double = 0.0
-
-//    var temp= spark.sparkContext.parallelize(List((0,0.0))).partitionBy(partitioner)
-
-
-    //   Run 10 iterations of PageRank
-     for (i <- 1 until 11) {
-       var contributions = adj_graph.join(ranks).flatMap{
+    var output = 0
+    //     Run 10 iterations of PageRank
+     for (i <- 0 until 10) {
+       val delta = ranks.lookup(0)
+       if (delta.isEmpty){dist_pr = 0.0} else {dist_pr = delta(0)/num_nodes}
+       val contributions = adj_graph.join(ranks).flatMap{
          case (v1, (v2, rank)) => v2.map(p => (p, rank / v2.size))
        }
-       contributions = contributions.reduceByKey(_+_)
-       // PageRank Mass Transfer
-       val delta = contributions.lookup(0)
-       val transferred_ranks = ranks.leftOuterJoin(contributions).filter(_._1 != 0)
-         .mapValues{ case (v, new_v) => new_v.getOrElse(0.0) }
-
-       if (delta.isEmpty) dist_pr = 0.0 else dist_pr = delta(0)/num_nodes
-
-       logger.error(dist_pr.toString+ "iter: " +  i.toString)
-       // transferred_ranks.saveAsTextFile("ranks"+i)
-       ranks = transferred_ranks.mapValues(v => 0.15/num_nodes + 0.85D*(dist_pr+v))
-       ranks.saveAsTextFile("ranks"+i)
-
-
+       ranks = contributions.reduceByKey((x,y)=> x+y).mapValues(v => 0.15/num_nodes + 0.85*(dist_pr+v))
      }
     // Write out the final ranks
-//    ranks.saveAsTextFile("ranks")
+     ranks.saveAsTextFile("ranks")
+//    print("\n+++++++++++++++++++++++++++++++++++++++++++++++\n")
+//    logger.info(ranks.collect().foreach(println))
+//    print("\n+++++++++++++++++++++++++++++++++++++++++++++++\n")
 
   }
 
-  def generateGraph(k:Int): List[(Int,Int)] = {
+  def generateGraph(k:Int): Seq[(Int,Int)] = {
     // Init empty scala collection
-    var G:List[(Int,Int)] = List.empty
+    var G:Seq[(Int,Int)] = Seq.empty
     for (i <- 1 to k*k) {
       if (i % k != 0) {
         G = G :+ (i, i + 1)
